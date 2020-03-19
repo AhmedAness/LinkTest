4b41c27c1e05bc050cf78e049805128e5c3b4594
ahmed aniss <ahmed_aness@ymail.com>
2020-03-19 15:37:15 +0200

    Add Back button to details activity

--- app/src/main/java/com/example/dt/linktest/View/Activities/DetailsActivity.java
+++ app/src/main/java/com/example/dt/linktest/View/Activities/DetailsActivity.java
@@ -3,12 +3,15 @@ package com.example.dt.linktest.View.Activities;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.dt.linktest.Model.articles;
@@ -30,8 +33,7 @@ public class DetailsActivity extends AppCompatActivity {
     TextView author;
     @BindView(R.id.description)
     TextView description;
-    @BindView(R.id.linearLayout3)
-    LinearLayout linearLayout3;
+
     @BindView(R.id.button)
     Button button;
     @BindView(R.id.PublishedAt)
@@ -44,14 +46,21 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
         ButterKnife.bind(this);
-
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
         viewModel = ViewModelProviders.of(this).get(MasterViewModel.class);
 
 
         articles = (com.example.dt.linktest.Model.articles) getIntent().getSerializableExtra("selecteditem");
-        Picasso.with(DetailsActivity.this).
-                load(articles.getUrlToImage()).into(imageview);
+        if (!articles.getUrlToImage().isEmpty()) {
+            imageview.setVisibility(View.VISIBLE);
+            Picasso.with(DetailsActivity.this).
+                    load(articles.getUrlToImage()).into(imageview);
+        }else {
+            imageview.setVisibility(View.GONE);
+        }
         Title.setText(articles.getTitle());
         author.setText(articles.getAuthor());
         description.setText(articles.getDescription());
@@ -62,5 +71,19 @@ public class DetailsActivity extends AppCompatActivity {
     public void onClick() {
         viewModel.OpenURL(Uri.parse(articles.getUrl()),DetailsActivity.this);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // todo: goto back activity from here
+
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
